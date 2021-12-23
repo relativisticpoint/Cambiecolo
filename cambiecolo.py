d02654eb7009ccde667434ab89c5d0feb7386af1
relativisticpoint <hsami272@gmail.com>
2021-12-23 02:02:36 +0100

    rajout de quelques commentaires

--- cambiecolo.py
+++ cambiecolo.py
@@ -1,30 +1,33 @@
 import multiprocessing
 import random
+global switcher 
+switcher = {0:'airplane',1:'car',2:'train',3:'bike',4:'shoes'}
 class Player:
     def __init__(self,id,hand) :
         self.id = id
         self.hand = hand 
     def __str__(self) -> str:
         return f"the hand of the player {self.id} is {self.hand} "
-
 def rand_hand():
     hand = []
-    switcher = {1:'airplane',2:'car',3:'train',4:'bike',5:'shoes'}
     for i in range(5):
-        aleatoire=random.randint(1,5)
+        aleatoire=random.randint(0,4)
         hand.append(switcher[aleatoire])
     print (hand)
     return hand
+def init_game(num_players):
+    p=[]
+    for i in range(num_players):
+        # p[i] = Process(target=Player, args=(i, rand_hand))
+        break
 
 
 
 
-def Game():
-    def init_game(num_players):
 
-        for i in range(num_players):
-            # p[i] = Process(target=Player, args=(i, rand_hand))
-            break
+def Game(num_players=2):
+    init_game(num_players)
+
 
 
 
@@ -33,6 +36,6 @@ def Game():
 
 if __name__ == "__main__":
     hand = ['airplane','car','train','bike','shoes']
-    arm = Player(5000,rand_hand())
-    print(arm)
+    armand = Player(5000,rand_hand())
+    print(armand)
 
